e7066387b99c5539ef33803abb2f0bf702ee6820
angelfullstack <angelfullstack@gmail.com>
2020-01-28 17:37:33 +0100

    iniciado js

--- js/main.js
+++ js/main.js
@@ -11,3 +11,25 @@ funciones:
         
 
 */
+
+var newTaskForm = document.querySelector('#newTaskForm')
+newTaskForm.addEventListener('submit',getForm)
+
+function paintTasks(){
+
+}
+
+function getForm(evt){
+    console.log(evt.target);
+    var nombre = evt.target[0].value;
+    console.log(nombre);
+
+}
+
+function createTask(){
+
+}
+
+function filterTasks(){
+
+}
